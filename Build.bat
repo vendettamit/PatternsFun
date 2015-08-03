d81ec73dd6362c5c06c75185935569f3b5309fa6
Amit Choudhary <vendett.amit@gmail.com>
2015-08-03 16:15:40 -0400

    Fixed issue in the build.bat

--- Build.bat
+++ Build.bat
@@ -14,11 +14,26 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Patterns.Cor.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+REM Unit tests
+call %nuget% install NUnit.Runners -Version 2.6.4 -OutputDirectory packages
+packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 YourSolution\ProjectA.Tests\bin\%config%\ProjectA.Tests.dll
+if not "%errorlevel%"=="0" goto failure
 
 mkdir Build
 mkdir Build\lib
-mkdir Build\lib\net40
 mkdir Build\lib\net45
 
-%nuget% pack "patterns.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+REM Package
+mkdir Build
+call %nuget% pack "Patterns.Cor\Patterns.Cor.csproj" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
+
+:success
+exit 0
+
+:failure
+exit -1
